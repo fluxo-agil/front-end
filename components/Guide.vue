56465a25e493d4c8b4226076ea54f04334b979dd
Irwin Schmitt <irwinschmitt@gmail.com>
2021-09-29 20:35:35 -0300

    Add home page

---
+++ components/Guide.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="text-h5 text-center py-8">Como utilizar</div>
+
+    <v-row>
+      <v-col v-for="item in items" :key="item.title" cols="12" md="4">
+        <div class="text-h6 text-center pb-4">
+          {{ item.title }}
+        </div>
+
+        <div class="text-body">
+          {{ item.content }}
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Título 1",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias ab quibusdam possimus corrupti consectetur adipisci cum sit! Corporis, neque pariatur! Harum fugiat reiciendis blanditiis quaerat ea? Natus odio voluptatibus commodi.",
+        },
+        {
+          title: "Título 2",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias ab quibusdam possimus corrupti consectetur adipisci cum sit! Corporis, neque pariatur! Harum fugiat reiciendis blanditiis quaerat ea? Natus odio voluptatibus commodi.",
+        },
+        {
+          title: "Título 3",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias ab quibusdam possimus corrupti consectetur adipisci cum sit! Corporis, neque pariatur! Harum fugiat reiciendis blanditiis quaerat ea? Natus odio voluptatibus commodi.",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ components/Upload.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col>
+      <v-card
+        class="pa-4 d-flex flex-column align-center justify-center"
+        min-height="50vh"
+        outlined
+      >
+        <DocumentIcon class="mb-4" />
+
+        <div class="mb-4">
+          Arraste seu
+          <span class="font-weight-bold">Histórico Escolar</span>
+          ou
+        </div>
+
+        <v-btn rounded depressed color="primary"> Selecione </v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import DocumentIcon from "@/assets/document.svg?inline";
+
+export default {
+  components: {
+    DocumentIcon,
+  },
+};
+</script>
