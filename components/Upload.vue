a42e9cfc99ce74cdbf0b306b5ca61ae146c4b294
Irwin Schmitt <irwinschmitt@gmail.com>
2021-09-29 21:39:57 -0300

    Add file upload

--- components/Upload.vue
+++ components/Upload.vue
@@ -14,7 +14,28 @@
           ou
         </div>
 
-        <v-btn rounded depressed color="primary"> Selecione </v-btn>
+        <v-btn
+          rounded
+          :depressed="selectedFile"
+          :outlined="!selectedFile"
+          color="primary"
+          max-width="100%"
+          @click="onButtonClick"
+        >
+          <span v-if="selectedFile">Enviar&nbsp;</span>
+
+          <span class="file__name">{{ buttonText }}</span>
+
+          <v-icon v-if="selectedFile" class="ml-2">mdi-cloud-upload</v-icon>
+        </v-btn>
+
+        <input
+          ref="uploader"
+          class="d-none"
+          type="file"
+          accept="application/pdf"
+          @change="onFileChanged"
+        />
       </v-card>
     </v-col>
   </v-row>
@@ -26,6 +47,35 @@ import DocumentIcon from "@/assets/document.svg?inline";
 export default {
   components: {
     DocumentIcon,
+  },
+
+  data() {
+    return {
+      selectedFile: null,
+      preco: 50,
+      defaultButtonText: "Selecione",
+    };
+  },
+
+  computed: {
+    buttonText() {
+      return this.selectedFile
+        ? this.selectedFile.name
+        : this.defaultButtonText;
+    },
+    buttonColor() {
+      return this.selectedFile && "primary";
+    },
+  },
+
+  methods: {
+    onButtonClick() {
+      this.$refs.uploader.click();
+    },
+
+    onFileChanged(event) {
+      this.selectedFile = event.target.files[0];
+    },
   },
 };
 </script>
